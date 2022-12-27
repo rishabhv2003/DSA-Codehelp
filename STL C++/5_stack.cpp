977515b6a709f2474f56998499e9547dd6951e79
Rishabh Verma <2000rishabhverma@gmail.com>
2022-12-27 06:35:08 +0530

    first commit

---
+++ STL C++/5_stack.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<stack>
+
+using namespace std;
+int main()
+{
+    stack <string> s;
+
+    s.push("rishabh");
+    s.push("hello");
+    s.push("verma");
+
+    return 0;
+}
