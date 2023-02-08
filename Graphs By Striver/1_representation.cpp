e923e2ce98fac5112904d69ccba4be89cf9f629f
rishabhv2003 <2000rishabhverma@gmail.com>
2023-02-08 22:49:59 +0530

    .

--- Graphs By Striver/1_representation.cpp
+++ Graphs By Striver/1_representation.cpp
@@ -6,6 +6,10 @@ int main()
 
     int n, m;
     cin >> n >> m;
+
+    /* n = number of nodes
+       m = number of edges */
+
     /* adjacency matrix */
     int adj[n + 1][m + 1];
     for (int i = 0; i < m; i++)
@@ -15,6 +19,18 @@ int main()
         adj[u][v] = 1;
         adj[v][u] = 1;
     }
+
     /* adjacency list */
+    vector<int> list[n + 1];
+    for (int i = 0; i < m; i++)
+    {
+        int u, v;
+        cin >> u >> v;
+        list[u].push_back(v);
+        list[v].push_back(u);
+
+        // if the graph is directed then we will remove the last line.
+    }
+
     return 0;
 }
